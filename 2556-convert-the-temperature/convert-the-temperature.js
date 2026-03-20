83616f1cdd27df1201c432db3e1146bcb6104b08
Doğukan Süme <161054688+CoderRoninn@users.noreply.github.com>
2026-03-20 23:02:01 +0300

    Time: 0 ms (100.00%) | Memory: 53.9 MB (31.85%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.js
+++ 2556-convert-the-temperature/convert-the-temperature.js
@@ -3,12 +3,17 @@
  * @return {number[]}
  */
 var convertTemperature = function(celsius) {
-    const kelvin = celsius + 273.15;
-    const fahrenheit = celsius * 1.8 + 32;
 
-    const ans = [];
+    //Declare kelvin and fahrenheit
+    const kelvin = celsius + 273.15;
+    const fahrenheit = celsius * 1.80 + 32.00;
+
+    //Declare an empty array of size 2 and put kelvin and hafrenheit values into it
+    const ans =  [];
     ans.push(kelvin);
     ans.push(fahrenheit);
+
+    //Return ans array
     return ans;
     
 };
